a3f5dab5a3b9d475ea9ed771f75b40f521a63a9e
chris.thompson <chris.thompson@tylertech.com>
2017-11-13 13:05:09 -0700

    Working on getting squares to change to x's and o's. This is broken right now.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   tiles = [
-    { text: 'One', cols: 1, rows: 1, color: 'lightblue' },
-    { text: 'Two', cols: 1, rows: 1, color: 'lightblue' },
-    { text: 'Three', cols: 1, rows: 1, color: 'lightblue' },
-    { text: 'Four', cols: 1, rows: 1, color: 'lightblue' },
-    { text: 'Five', cols: 1, rows: 1, color: 'lightblue' },
-    { text: 'Six', cols: 1, rows: 1, color: 'lightblue' },
-    { text: 'Seven', cols: 1, rows: 1, color: 'lightblue' },
-    { text: 'Eight', cols: 1, rows: 1, color: 'lightblue' },
-    { text: 'Nine', cols: 1, rows: 1, color: 'lightblue' }
+    { text: 'One', cols: 1, rows: 1, color: 'lightblue', imagePath: '' },
+    { text: 'Two', cols: 1, rows: 1, color: 'lightblue', imagePath: '' },
+    { text: 'Three', cols: 1, rows: 1, color: 'lightblue', imagePath: '' },
+    { text: 'Four', cols: 1, rows: 1, color: 'lightblue', imagePath: '' },
+    { text: 'Five', cols: 1, rows: 1, color: 'lightblue', imagePath: '' },
+    { text: 'Six', cols: 1, rows: 1, color: 'lightblue', imagePath: '' },
+    { text: 'Seven', cols: 1, rows: 1, color: 'lightblue', imagePath: '' },
+    { text: 'Eight', cols: 1, rows: 1, color: 'lightblue', imagePath: '' },
+    { text: 'Nine', cols: 1, rows: 1, color: 'lightblue', imagePath: '' }
   ];
+  // public xImage = require("./Images/x.png");
 
   placeSymbol(tile) {
-    tile.color = 'red';
-    // console.log(tile);
+    tile.imagePath = '../Images/x.png';
+    console.log(tile);
 }}
