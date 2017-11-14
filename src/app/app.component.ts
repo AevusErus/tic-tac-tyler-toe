9ca5bab6e0581a5ad0e7f6869eb75cde1f2e79e2
Zipp <christopher.zipp@tylertech.com>
2017-11-14 08:17:45 -0700

    Added array to keep track of board, don't allow playing on a tile that has a piece

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public playerBool = true;
+  n: number = 3;
+  movecount: number = 0;
+  // declare board array to keep track of moves, initialize it with 'B' blank entries
+  board: String[][] = [["B","B","B"],["B","B","B"],["B","B","B"]];
+
+
   tiles = [
     { text: 'One', cols: 1, rows: 1, color: 'lightblue', imagePath: '', row: 1 , column: 1 },
     { text: 'Two', cols: 1, rows: 1, color: 'lightblue', imagePath: '', row: 1, column: 2  },
@@ -18,18 +24,35 @@ export class AppComponent {
     { text: 'Eight', cols: 1, rows: 1, color: 'lightblue', imagePath: '', row: 3, column: 2  },
     { text: 'Nine', cols: 1, rows: 1, color: 'lightblue', imagePath: '', row: 3, column: 3 }
   ];
-  // public xImage = require("./Images/x.png");
 
   placeSymbol(tile) {
-    if ( this.playerBool ) {
-    tile.imagePath = '../assets/x.png';
+    // player X's turn
+    if (this.playerBool) {
+      //check if the tile is blank
+      if (this.board[tile.column-1][tile.row-1] == "B") {
+        tile.imagePath = '../assets/x.png';
+        this.board[tile.column-1][tile.row-1] = "X";
+        this.movecount++;
+        this.playerBool = !this.playerBool;
+      } else {
+        console.log("Must pick a blank space! It is still X's turn.");
+      }
+    // player Y's turn  
     } else {
-      tile.imagePath = '../assets/0.png';
+      //check if the tile is blank
+      if (this.board[tile.column-1][tile.row-1] == "B") {
+        tile.imagePath = '../assets/0.png';
+        this.board[tile.column-1][tile.row-1] = "O";
+        this.movecount++;
+        this.playerBool = !this.playerBool;
+      } else {
+        console.log("Must pick a blank space! It is still O's turn.");
+      } 
     }
-    this.playerBool = !this.playerBool;
-    // console.log(tile);
+    //display the board array
+    console.log(this.board[0][0], this.board[1][0], this.board[2][0]);
+    console.log(this.board[0][1], this.board[1][1], this.board[2][1]);
+    console.log(this.board[0][2], this.board[1][2], this.board[2][2]);
+    console.log("Number of moves:"+this.movecount);
 }
-  evaluate(value) {
-    console.log(value);
-  }
 }
