d176651753e0b29ba26e8859ee22bfcf1b6228cc
chris.thompson <chris.thompson@tylertech.com>
2017-11-13 17:07:03 -0700

    Moving the images to the asset folder. Also got the implementation for the images to draw on the squares done. We just need to implement the win condition.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public playerBool = true;
   tiles = [
-    { text: 'One', cols: 1, rows: 1, color: 'lightblue', imagePath: '' },
-    { text: 'Two', cols: 1, rows: 1, color: 'lightblue', imagePath: '' },
-    { text: 'Three', cols: 1, rows: 1, color: 'lightblue', imagePath: '' },
-    { text: 'Four', cols: 1, rows: 1, color: 'lightblue', imagePath: '' },
-    { text: 'Five', cols: 1, rows: 1, color: 'lightblue', imagePath: '' },
-    { text: 'Six', cols: 1, rows: 1, color: 'lightblue', imagePath: '' },
-    { text: 'Seven', cols: 1, rows: 1, color: 'lightblue', imagePath: '' },
-    { text: 'Eight', cols: 1, rows: 1, color: 'lightblue', imagePath: '' },
-    { text: 'Nine', cols: 1, rows: 1, color: 'lightblue', imagePath: '' }
+    { text: 'One', cols: 1, rows: 1, color: 'lightblue', imagePath: '', row: 1 , column: 1 },
+    { text: 'Two', cols: 1, rows: 1, color: 'lightblue', imagePath: '', row: 1, column: 2  },
+    { text: 'Three', cols: 1, rows: 1, color: 'lightblue', imagePath: '', row: 1, column: 3  },
+    { text: 'Four', cols: 1, rows: 1, color: 'lightblue', imagePath: '', row: 2, column: 1  },
+    { text: 'Five', cols: 1, rows: 1, color: 'lightblue', imagePath: '', row: 2, column: 2  },
+    { text: 'Six', cols: 1, rows: 1, color: 'lightblue', imagePath: '', row: 2, column: 3  },
+    { text: 'Seven', cols: 1, rows: 1, color: 'lightblue', imagePath: '', row: 3, column: 1  },
+    { text: 'Eight', cols: 1, rows: 1, color: 'lightblue', imagePath: '', row: 3, column: 2  },
+    { text: 'Nine', cols: 1, rows: 1, color: 'lightblue', imagePath: '', row: 3, column: 3 }
   ];
   // public xImage = require("./Images/x.png");
 
   placeSymbol(tile) {
-    tile.imagePath = '../Images/x.png';
-    console.log(tile);
-}}
+    if ( this.playerBool ) {
+    tile.imagePath = '../assets/x.png';
+    } else {
+      tile.imagePath = '../assets/0.png';
+    }
+    this.playerBool = !this.playerBool;
+    // console.log(tile);
+}
+  evaluate(value) {
+    console.log(value);
+  }
+}
